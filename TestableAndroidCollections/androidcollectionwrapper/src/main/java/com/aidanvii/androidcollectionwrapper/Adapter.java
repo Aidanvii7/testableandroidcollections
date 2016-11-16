ec3659c270393dc38fa08f88c5636b5717a24893
Aidan McWilliams <aidan.mcwilliams@vimn.com>
2016-11-16 10:22:20 +0100

    removed app module
    app module not needed

--- TestableAndroidCollections/androidcollectionwrapper/src/main/java/com/aidanvii/androidcollectionwrapper/Adapter.java
+++ TestableAndroidCollections/androidcollectionwrapper/src/main/java/com/aidanvii/androidcollectionwrapper/Adapter.java
@@ -3,7 +3,6 @@ package com.aidanvii.androidcollectionwrapper;
 /**
  * Created by aidan.mcwilliams@vimn.com on 15/11/16.
  */
-
 public final class Adapter {
 
     private final SparseArrayWrapper<String> sparseArrayWrapper;
